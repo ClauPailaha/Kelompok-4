9c662ab0e4fd670b2323145ddd8085804d1e2d49
Rianakeni <s22210372@student.unklab.ac.id>
2024-02-22 23:08:24 +0800

    updated by Riana

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+} from 'react-native';
+import Regis from './Src/Components/Registration/Regis';
+import List from './Src/Components/Userlist/List';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Regis/>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+});
+
+export default App;
