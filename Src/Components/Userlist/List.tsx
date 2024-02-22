3f0390c6558cd6cc190cf402ee045fd40db702e6
arturrr03 <rawungito@gmail>
2024-02-22 23:04:51 +0800

    Update by artur

--- Src/Components/Userlist/List.tsx
+++ Src/Components/Userlist/List.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const List = () => {
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     axios
       .get('https://jsonplaceholder.typicode.com/users')
@@ -36,3 +37,50 @@ const List = () => {
       </View>
     );
   };
+
+  return (
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.th}>User List</Text>
+      </View>
+      <FlatList
+        data={users}
+        keyExtractor={item => item.id.toString()}
+        renderItem={renderUserCard}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 10,
+    borderWidth: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: 'black',
+  },
+  content: {
+    fontSize: 18,
+    color: 'black',
+    fontWeight: 'normal',
+  },
+  header: {
+    marginBottom: 20,
+    marginLeft: 10,
+  },
+  th: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'black',
+    margin: 10,
+  },
+});
+
+export default List;
